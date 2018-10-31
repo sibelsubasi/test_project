96a277c87470b4dbba11767896a49f9d8a5bcf2d
sibelsubasi <subasi.sibel@gmail.com>
2018-10-31 16:46:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+// jquery: burger menu aktif/pasif
+  $(document).ready(function(){
+      $("#toggle").click(function() {
+        $(".burger-wrapper").toggleClass("active");
+      });
+    });
